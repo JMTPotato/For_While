78d4dd1ebb229d7c196b09d0a2569e5b04855076
JMTPotato <KoreaPotato03@gmail.com>
2022-12-09 01:35:03 +0900

    자기 전에

--- 11022/11022.c
+++ 11022/11022.c
@@ -3,8 +3,16 @@
 
 int main()
 {
-	int x;
+	int T, A, B, C = 0;
 
+	scanf("%d", &T);
+
+	for (int x = 1; x <= T; x++)
+	{
+		scanf("%d %d", &A, &B);
+		C = A + B;
+		printf("Case #%d: %d + %d = %d\n", x, A, B, C);
+	}
 
 	return 0;
 }
